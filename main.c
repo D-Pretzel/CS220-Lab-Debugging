e26c18f03eafe5f9280ca87abc0cbb04138e002d
David Petzold <petzoldavid02@gmail.com>
2023-01-08 20:46:51 -0700

    I don't particularly like debugging

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-/** 
+/**
  * ===========================================================
- * Name:
- * Section:
+ * Name: David Petzold
+ * Section: M3
  * Project: Practice debugging lab.
  * ===========================================================
  */
@@ -13,11 +13,12 @@
  * and store in an array of integers.  It should prompt the
  * user to enter a value, then use binary search to determine
  * if the value is in the array.
- * 
+ *
  * There are errors present in this code you should fix them.
  ****************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX_NUMBERS 1000 /* Max numbers in file */
 
 const char DATA_FILE[] = "numbers.dat"; /* File with numbers */
@@ -44,11 +45,12 @@ int main() {
 
     max_count = 0;
     while (1) {
-        if (fgets(line, sizeof(line), in_file) == NULL)
-            break;
+        if (fgets(line, sizeof(line), in_file) == NULL) {
+            break;  /* End of file */
+        }
 
         /* convert number */
-        sscanf(line, "%d", data[max_count]);
+        sscanf(line, "%d", &data[max_count]);
         ++max_count;
     }
 
@@ -57,29 +59,29 @@ int main() {
         fgets(line, sizeof(line), stdin);
         sscanf(line, "%d", &search);
 
-        if (search == -1)
+        if (search == -1) {
             break;
+        }
 
         low = 0;
         high = max_count;
-
         while (1) {
             middle = (low + high) / 2;
 
-            if (data[middle] == search) {
+            if (search == data[middle]) {
                 printf("Found at index %d\n", middle);
+                break;
+            } else if (search > data[middle]) { // Search is on the right side of middle
+                low = middle;   // Move low to be middle
+            } else {    // Search is on the left side of middle
+                high = middle;  // Move high to be middle
             }
 
-            if (low == high) {
+            if (low == high || low < 0 || high < 0) {
                 printf("Not found\n");
                 break;
             }
-
-            if (data[middle] < search)
-                low = middle;
-            else
-                high = middle;
         }
     }
-    return (0);
+    return 0;
 }
